345f378a9bb40c71c5c80a36e3b322176649465c
JGSEIXAS <jgss@ic.ufal.br>
2025-06-20 00:10:15 -0300

    UB- Binary Search

---
+++ upper_bound.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> vetor(10, 5);
+
+int upperbound(int l, int r, int x) {
+    while (l < r) {
+        int mid = (l + r) / 2;
+        if (vetor[mid] <= x)
+            l = mid + 1;
+        else
+            r = mid;
+    }
+    return l;  // índice do primeiro elemento maior que x
+}
+
+int main(){
+    for(int i=0; i<5; i++){
+        vetor[i]=0;
+    }
+    int x = 5;
+
+    cout << upperbound(0, vetor.size(), x) << endl;
+    cout << (upper_bound(vetor.begin(), vetor.end(), x) - vetor.begin()) << endl; // converte iterador para índice
+}

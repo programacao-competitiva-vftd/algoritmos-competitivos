a7bca30d03193bdd70baf3b0a18fab110dfb6cdc
JGSEIXAS <jgss@ic.ufal.br>
2025-06-20 00:21:46 -0300

    UB - BinarySearch

--- upper_bound.cpp
+++ upper_bound.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+//Quero saber o primeiro elemento que não satifaz uma condição (upperbound)
 vector<int> vetor(10, 5);
 
 int upperbound(int l, int r, int x) {
@@ -18,8 +18,8 @@ int main(){
     for(int i=0; i<5; i++){
         vetor[i]=0;
     }
-    int x = 5;
-
-    cout << upperbound(0, vetor.size(), x) << endl;
+    int x = 0;
+    //0 0 0 0 0 5 5 5 5 5
+    cout << upperbound(0, vetor.size(), x) << endl; 
     cout << (upper_bound(vetor.begin(), vetor.end(), x) - vetor.begin()) << endl; // converte iterador para índice
 }
